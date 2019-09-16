87d5b760e3ade74dd81ded8dd65065cf0a25bb4d
Bruno <brodrigues1990@gmail.com>
2019-09-16 18:39:50 -0300

    Ajuste na rota de detalhe de personagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'personagens', pathMatch: 'full' },
   { path: 'personagens', loadChildren: () => import('./pages/personagens/personagens.module').then( m => m.PersonagensPageModule)},
-  { path: 'detalhe-personagem', loadChildren: './pages/detalhe-personagem/detalhe-personagem.module#DetalhePersonagemPageModule' }
+  { path: 'detalhe-personagem', loadChildren: () => import('./pages/detalhe-personagem/detalhe-personagem.module').then( m => m.DetalhePersonagemPageModule)},
 ];
 
 @NgModule({
